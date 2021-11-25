72bd794f99cb187ae6f8b4b3ffa41cbad1085e70
CodeSk3tch <6042043+CodeSk3tch@users.noreply.github.com>
2021-11-25 21:19:47 +1100

    Add files via upload

---
+++ src/Main.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace HaloInfiniteServerBlock
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ApplyButton = new System.Windows.Forms.Button();
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // ApplyButton
+            // 
+            this.ApplyButton.Location = new System.Drawing.Point(264, 659);
+            this.ApplyButton.Margin = new System.Windows.Forms.Padding(4);
+            this.ApplyButton.Name = "ApplyButton";
+            this.ApplyButton.Size = new System.Drawing.Size(181, 28);
+            this.ApplyButton.TabIndex = 1;
+            this.ApplyButton.Text = "Apply Changes";
+            this.toolTip1.SetToolTip(this.ApplyButton, "Update blocked servers. Writes changes to hosts file");
+            this.ApplyButton.UseVisualStyleBackColor = true;
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Location = new System.Drawing.Point(156, 659);
+            this.RefreshButton.Margin = new System.Windows.Forms.Padding(4);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(100, 28);
+            this.RefreshButton.TabIndex = 2;
+            this.RefreshButton.Text = "Refresh";
+            this.toolTip1.SetToolTip(this.RefreshButton, "Reload server list and hosts file. Updates UI data");
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(16, 15);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(428, 636);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Block";
+            this.columnHeader1.Width = 39;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Region";
+            this.columnHeader2.Width = 110;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Location";
+            this.columnHeader3.Width = 152;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 700);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.RefreshButton);
+            this.Controls.Add(this.ApplyButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Halo:Infinite Server Block";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ApplyButton;
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ src/Main.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Web.Script.Serialization;
+using System.Net;
+
+namespace HaloInfiniteServerBlock
+{
+    public partial class Main : Form
+    {
+        private string HostsFile = "C:\\Windows\\System32\\drivers\\etc\\hosts";
+        private string LocalServerFile = "servers.json";
+        private List<Server> ServerList;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            Shown += (s, e) =>
+            {
+                if (!File.Exists(LocalServerFile))
+                {
+                    MessageBox.Show("Failed to locate the server list file\n\nCheck https://github.com/CodeSk3tch/HaloInfinite-Server-Block updates and help\n\nApplication will now exit", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Application.Exit();
+                }
+                else
+                {
+                    ReadServerListFile(ref ServerList);
+                    UpdateServerList(ref ServerList);
+                    SetServerListToUI(ref ServerList);
+                }
+            };
+
+            ApplyButton.Click += (s, e) =>
+            {
+                GetServerListFromUI(ref ServerList);
+                UpdateHostsFile(ref ServerList);
+                UpdateServerList(ref ServerList);
+                SetServerListToUI(ref ServerList);
+                MessageBox.Show("Hosts file has been updated", "Changes applied", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            };
+
+            RefreshButton.Click += (s, e) =>
+            {
+                ReadServerListFile(ref ServerList);
+                UpdateServerList(ref ServerList);
+                SetServerListToUI(ref ServerList);
+            };
+        }
+
+        private void SetServerListToUI(ref List<Server> servers)
+        {
+            listView1.BeginUpdate();
+            listView1.Items.Clear();
+            listView1.Groups.Clear();
+            ListViewItem item;
+            ListViewGroup group;
+            foreach (var server in servers)
+            {
+                item = new ListViewItem("");
+                item.SubItems.Add(server.InfiniteServer.Region);
+                item.SubItems.Add(server.InfiniteServer.Location);
+                item.Checked = server.IsBlocked;
+
+                group = listView1.Groups[server.InfiniteServer.Geography];
+                if (group == null)
+                {
+                    group = new ListViewGroup(server.InfiniteServer.Geography, server.InfiniteServer.Geography);
+                    listView1.Groups.Add(group);
+                    item.Group = group;
+                }
+                item.Group = group;
+                listView1.Items.Add(item);
+            }
+            listView1.EndUpdate();
+        }
+
+        private void GetServerListFromUI(ref List<Server> servers)
+        {
+            if (listView1.Items.Count == 0) return;
+            foreach (var server in servers)
+            {
+                foreach (ListViewItem item in listView1.Items)
+                {
+                    if (item.SubItems[1].Text == server.InfiniteServer.Region)
+                    {
+                        server.IsBlocked = item.Checked;
+                    }
+                }
+            }
+        }
+
+        private void DisableReadOnlyHostsFile()
+        {
+            FileInfo file = new FileInfo(HostsFile);
+            if (file.Exists && file.IsReadOnly)
+            {
+                File.SetAttributes(HostsFile, (File.GetAttributes(HostsFile) & ~FileAttributes.ReadOnly));
+            }
+        }
+
+        private bool ReadServerListFile(ref List<Server> servers)
+        {
+            FileInfo file = new FileInfo(LocalServerFile);
+            if (!file.Exists) return false;
+            servers = new List<Server>();
+            string buffer;
+            using (var stream = file.OpenText())
+            {
+                buffer = stream.ReadToEnd();
+                stream.Close();
+            }
+            var temp = new JavaScriptSerializer().Deserialize<List<InfiniteServer>>(buffer);
+            foreach (var t in temp)
+            {
+                servers.Add(new Server() { InfiniteServer = t, IsBlocked = false });
+            }
+            return true;
+        }
+
+        private void UpdateServerList(ref List<Server> servers)
+        {
+            List<string> buffer = new List<string>();
+            using (var stream = new StreamReader(HostsFile, Encoding.UTF8))
+            {
+                buffer.AddRange(stream.ReadToEnd().Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries));
+                stream.Close();
+            }
+            foreach (var server in servers)
+            {
+                server.IsBlocked = buffer.ContainsAll(server.HostEntries);
+            }
+        }
+
+        private void UpdateHostsFile(ref List<Server> servers)
+        {
+            DisableReadOnlyHostsFile();
+            List<string> buffer = new List<string>();
+            using (var stream = new StreamReader(HostsFile, Encoding.UTF8))
+            {
+                buffer.AddRange(stream.ReadToEnd().Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries));
+                stream.Close();
+            }
+            foreach (var server in servers)
+            {
+                if (server.IsBlocked)
+                {
+                    if (!buffer.ContainsAll(server.HostEntries))
+                    {
+                        foreach (var entry in server.HostEntries)
+                        {
+                            if (!buffer.Contains(entry)) buffer.Add(entry);
+                        }
+                    }
+                }
+                else
+                {
+                    if (buffer.ContainsAny(server.HostEntries))
+                    {
+                        foreach (var entry in server.HostEntries)
+                        {
+                            if (buffer.Contains(entry)) buffer.RemoveAll(e => e == entry);
+                        }
+                    }
+                }
+            }
+            using (var stream = new StreamWriter(File.Open(HostsFile, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite), Encoding.UTF8))
+            {
+                foreach (var line in buffer)
+                {
+                    stream.WriteLine(line);
+                }
+                stream.Flush();
+                stream.Close();
+            }
+        }
+
+        public class Server
+        {
+            public InfiniteServer InfiniteServer;
+            public bool IsBlocked;
+            public string[] HostEntries
+            {
+                get
+                {
+                    var count = InfiniteServer.Hostnames.Count;
+                    if (count == 0) return null;
+                    var result = new string[count];
+                    for (int i = 0; i < count; i++)
+                    {
+                        result[i] = $"0.0.0.0 {InfiniteServer.Hostnames[i]}";
+                    }
+                    return result;
+                }
+            }
+        }
+
+        public class InfiniteServer
+        {
+            public string Geography;
+            public string Region;
+            public string Location;
+            public List<string> Hostnames;
+        }
+
+    }
+
+    public static class Extensions
+    {
+        public static bool ContainsAll(this List<string> a, params string[] b)
+        {
+            foreach (var x in b)
+            {
+                if (!a.Contains(x)) return false;
+            }
+            return true;
+        }
+
+        public static bool ContainsAny(this List<string> a, params string[] b)
+        {
+            foreach (var x in b)
+            {
+                if (a.Contains(x)) return true;
+            }
+            return true;
+        }
+    }
+
+}
